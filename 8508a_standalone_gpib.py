a02ee12f6026d20d97cf207c78e6ec16d5b73939
reps <marco@reps.cc>
2024-07-18 14:28:43 +0200

    Update 8508a_standalone_gpib.py

--- 8508a_standalone_gpib.py
+++ 8508a_standalone_gpib.py
@@ -8,8 +8,8 @@ my_instrument.write('*RST')
 my_instrument.write('*CLS')
 time.sleep(3)
 print(my_instrument.query('*IDN?'))
-dmm.write("DCV 10,FILT_OFF,RESL8,FAST_ON,TWO_WR")
-dmm.write("ZERO?")
+my_instrument.write("DCV 10,FILT_OFF,RESL8,FAST_ON,TWO_WR")
+my_instrument.write("ZERO?")
 
 timestr = time.strftime("%Y%m%d-%H%M%S_")
 with open('csv/'+timestr+'F_8508A_short_10V_FAST_ON.csv', mode='w') as csv_file:
